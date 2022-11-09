cdea4a2e59138c47d3f9c10ecfd0c5df75a3c148
Aviral Goel <aviralgoel@rocketmail.com>
2022-11-08 23:16:31 -0800

    added progress gif

--- MyProject/src/Main.c
+++ MyProject/src/Main.c
@@ -35,7 +35,7 @@ void setup(void) {
 		window_height
 	);
 	//load_cube_mesh_data();
-	load_obj_file_data("./assets/cube.obj");
+	load_obj_file_data("./assets/f22.obj");
 }
 
 void process_input(void) {
